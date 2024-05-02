a6a022ec2c824e36961b3dba51588989317860cc
darryl <darrylestrada97@gmail.com>
2024-05-02 10:29:10 +0200

    added bullsEye icon to Success rate

--- app/Components/QuizCard.js
+++ app/Components/QuizCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import Image from "next/image";
-import {faCode, faEllipsis, faPlay} from '@fortawesome/free-solid-svg-icons';
+import {faBullseye, faCircleCheck, faCode, faEllipsis, faPlay} from '@fortawesome/free-solid-svg-icons';
 
 
 function QuizCard(props) {
     return (
-        <div className="rounded-[10px] flex flex-col gap-2 border bg-amber-300 bg-white p-4 max-h-80 max-w-80">
+        <div className="rounded-[10px] flex flex-col gap-2 border bg-amber-300 bg-white p-4 max-h-96 max-w-96">
             <div className="relative bg-green-700 w-full h-32 flex justify-center items-center rounded-md">
                 <div className="absolute cursor-pointer top-3 right-3">
                     <FontAwesomeIcon
@@ -27,10 +27,16 @@ function QuizCard(props) {
             <p className="text-sm font-light">Description of the quiz</p>
             <div className="flex gap-3">
                 <div className="flex gap-1 items-center">
-                    <Image src={"/img/quiz.svg"} width={20} height={20} alt="quiz icon"/>
+                   {/**Icon for success rate*/}
+                    <FontAwesomeIcon
+                        icon={faBullseye}
+                        width={20}
+                        height={20}
+                        className="text-green-500"
+                    />
                     <span className={"text-[12px]"}>Success rate: 100%</span>
                 </div>
-                <div className={"rounded-full w-7 h-7 bg-green-700 flex justify-center items-center"}>
+                <div className={"rounded-full w-7 h-7 bg-green-700 flex justify-center items-center cursor-pointer"}>
                     <FontAwesomeIcon
                         icon={faPlay}
                         className={"text-white"}
